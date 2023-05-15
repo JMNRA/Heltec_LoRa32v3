80b5ee9785777e5590d46b1c4390ff6ba6588d24
Josue Medina <joshrm@Omen>
2023-05-14 20:56:48 -0500

    Cm 0

---
+++ src/main.cpp
@@ -0,0 +1,111 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <RadioLib.h>
+#include <U8g2lib.h>
+
+// Heltec LoRa32 v3 OLED PINS
+#define OLED_SDA 17
+#define OLED_SCL 18
+#define OLED_RST 21
+
+// Heltec LoRa32 v3 (SX1262) LoRa PINS
+#define LORA_SCK 9
+#define LORA_MISO 11
+#define LORA_MOSI 10
+#define LORA_CS 8 // LORA_NSS
+#define LORA_DIO1 14
+#define LORA_NRST 12
+#define LORA_BUSY 13
+
+SX1262 radio = new Module(LORA_CS, LORA_DIO1, LORA_NRST, LORA_BUSY);
+
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/OLED_SCL, /* data=*/OLED_SDA, /* reset=*/OLED_RST);
+// U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+// U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 16, /* data=*/ 17, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
+
+void initLoRa()
+{
+  Serial.println("Inicializando LoRa...");
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+
+  int state = radio.begin();
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    Serial.println(F("EXITO!"));
+  }
+  else
+  {
+    Serial.println(F("ERROR , CODE : "));
+    Serial.println(state);
+    for (;;)
+      ;
+  }
+}
+
+void holamundolorareceiver()
+{
+  Serial.print(F("[SX1262] Waiting for incoming transmission ... "));
+
+  // you can receive data as an Arduino String
+  // NOTE: receive() is a blocking method!
+  //       See example ReceiveInterrupt for details
+  //       on non-blocking reception method.
+  String str;
+  int state = radio.receive(str);
+
+  // you can also receive data as byte array
+  /*
+    byte byteArr[8];
+    int state = radio.receive(byteArr, 8);
+  */
+
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    // packet was successfully received
+    Serial.println(F("success!"));
+
+    // print the data of the packet
+    Serial.print(F("[SX1262] Data:\t\t"));
+    Serial.println(str);
+
+    // print the RSSI (Received Signal Strength Indicator)
+    // of the last received packet
+    Serial.print(F("[SX1262] RSSI:\t\t"));
+    Serial.print(radio.getRSSI());
+    Serial.println(F(" dBm"));
+
+    // print the SNR (Signal-to-Noise Ratio)
+    // of the last received packet
+    Serial.print(F("[SX1262] SNR:\t\t"));
+    Serial.print(radio.getSNR());
+    Serial.println(F(" dB"));
+  }
+  else if (state == RADIOLIB_ERR_RX_TIMEOUT)
+  {
+    // timeout occurred while waiting for a packet
+    Serial.println(F("timeout!"));
+  }
+  else if (state == RADIOLIB_ERR_CRC_MISMATCH)
+  {
+    // packet was received, but is malformed
+    Serial.println(F("CRC error!"));
+  }
+  else
+  {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  initLoRa();
+}
+
+void loop()
+{
+  holamundolorareceiver();
+}
